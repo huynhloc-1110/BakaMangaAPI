65439574c5bd8fc19c1290c9c3bfdedceeb78273
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2023-08-28 15:34:47 +0700

    Implement GetUserBasicInfo for a specific user id

--- BakaMangaAPI/Controllers/Account/UserController.cs
+++ BakaMangaAPI/Controllers/Account/UserController.cs
@@ -4,24 +4,29 @@ using Microsoft.AspNetCore.Identity;
 using AutoMapper;
 using BakaMangaAPI.Models;
 using BakaMangaAPI.DTOs;
+using BakaMangaAPI.Data;
 
 namespace BakaMangaAPI.Controllers;
 
 [Route("account/user")]
 [ApiController]
-[Authorize]
 public class UserController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IMapper _mapper;
-    public UserController(UserManager<ApplicationUser> userManager, IMapper mapper)
+    private readonly ApplicationDbContext _context;
+
+    public UserController(UserManager<ApplicationUser> userManager,
+        ApplicationDbContext context, IMapper mapper)
     {
         _userManager = userManager;
         _mapper = mapper;
+        _context = context;
     }
 
-    [HttpGet]
-    public async Task<UserBasicDTO> GetCurrentUserBasicInfo()
+    [HttpGet("me")]
+    [Authorize]
+    public async Task<IActionResult> GetCurrentUserBasicInfo()
     {
         var currentUser = await _userManager.GetUserAsync(User);
         var userRoles = await _userManager.GetRolesAsync(currentUser);
@@ -29,6 +34,22 @@ public class UserController : ControllerBase
         var userBasicDto = _mapper.Map<UserBasicDTO>(currentUser);
         userBasicDto.Roles = userRoles.ToList();
 
-        return userBasicDto;
+        return Ok(userBasicDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserBasicInfo(string id)
+    {
+        var user = await _context.ApplicationUsers.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        var userBasicDto = _mapper.Map<UserBasicDTO>(user);
+        userBasicDto.Roles = userRoles.ToList();
+
+        return Ok(userBasicDto);
     }
 }
