d704adbd764f1b8cc607131e93820951f2235433
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2023-10-06 09:04:16 +0700

    Update post controller
    - Refactor code
    - Return post after PUT

--- BakaMangaAPI/Controllers/Community/PostController.cs
+++ BakaMangaAPI/Controllers/Community/PostController.cs
@@ -94,72 +94,77 @@ public class PostController : ControllerBase
     [Authorize]
     public async Task<IActionResult> PutPost(string postId, [FromForm] PostEditDTO dto)
     {
-        // validate
         var post = await _context.Posts
             .Include(p => p.User)
             .Include(p => p.Images)
             .SingleOrDefaultAsync(p => p.Id == postId);
+
+        // validate
         if (post == null)
         {
             return NotFound();
         }
-
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (post.User.Id != userId)
+        if (!IsPostOwner(post, User))
         {
             return Forbid();
         }
 
-        // change properties
         post.Content = dto.Content;
-
-        foreach (var image in post.Images)
-        {
-            await _mediaManager.DeleteImageAsync(image.Path);
-        }
-        _context.Images.RemoveRange(post.Images);
-
-        post.Images = new();
-        for (int i = 0; i < dto.Images.Count; i++)
-        {
-            var image = dto.Images[i];
-            var imageId = Guid.NewGuid().ToString();
-            var imagePath = await _mediaManager.UploadImageAsync(image, imageId, ImageType.Post);
-            post.Images.Add(new() { Id = imageId, Number = i, Path = imagePath });
-        }
-
+        await DeleteAllImagesFrom(post);
+        await AddImagesTo(post, dto.Images);
         await _context.SaveChangesAsync();
-        return NoContent();
+        return Ok(_mapper.Map<PostBasicDTO>(post));
     }
 
     [HttpDelete("{postId}")]
     [Authorize]
     public async Task<IActionResult> DeletePost(string postId)
     {
-        // validate
         var post = await _context.Posts
             .Include(p => p.User)
             .Include(p => p.Images)
             .SingleOrDefaultAsync(p => p.Id == postId);
+
+        // validate
         if (post == null)
         {
             return NotFound();
         }
-
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (post.User.Id != userId)
+        if (!IsPostOwner(post, User))
         {
             return Forbid();
         }
 
+        await DeleteAllImagesFrom(post);
+        _context.Posts.Remove(post);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    private bool IsPostOwner(Post post, ClaimsPrincipal principal)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return post.User.Id == userId;
+    }
+
+    private async Task DeleteAllImagesFrom(Post post)
+    {
         foreach (var image in post.Images)
         {
             await _mediaManager.DeleteImageAsync(image.Path);
         }
-
         _context.Images.RemoveRange(post.Images);
-        _context.Posts.Remove(post);
-        await _context.SaveChangesAsync();
-        return NoContent();
+    }
+
+    private async Task AddImagesTo(Post post, List<IFormFile> images)
+    {
+        post.Images = new();
+        for (int i = 0; i < images.Count; i++)
+        {
+            var image = images[i];
+            var imageId = Guid.NewGuid().ToString();
+            var imagePath = await _mediaManager.UploadImageAsync(image, imageId, ImageType.Post);
+            post.Images.Add(new() { Id = imageId, Number = i, Path = imagePath });
+        }
     }
 }
