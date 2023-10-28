d5c855097f72297509a2587f9ee852bd4ef7672d
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2023-10-28 14:52:26 +0700

    Remove unnecessary files

--- BakaMangaAPITest/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using BakaMangaAPI.Data;
-global using BakaMangaAPI.DTOs;
-global using BakaMangaAPI.Models;
-global using BakaMangaAPITest.Fixtures;
-global using BakaMangaAPITest.Mocks;
-global using BakaMangaAPI.Controllers;
-global using BakaMangaAPI.Services;
